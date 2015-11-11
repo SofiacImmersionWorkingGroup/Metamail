e781968d278deb57af2e1965e1988b9eb86f3ced
Mr-Snailman <dustin.saunders@sofiac.us>
2015-11-11 15:43:33 -0500

    Added spring configuration for services and other configurable objects.

--- enron-importer/src/main/java/com/igalia/enron_importer/thread/MainThread.java
+++ enron-importer/src/main/java/com/igalia/enron_importer/thread/MainThread.java
@@ -1,6 +1,20 @@
 package com.igalia.enron_importer.thread;
 
+import com.igalia.enron_importer.services.MailFactory;
+import com.igalia.enron_importer.services.MailRepository;
+
+import javax.annotation.Resource;
+
 public class MainThread implements Runnable {
+
+  @Resource
+  private MailFactory mailFactory;
+
+  @Resource
+  private MailRepository mailRepository;
+
+  @Resource(name="maildir")
+  private String maildir;
 
   @Override
   public void run() {
